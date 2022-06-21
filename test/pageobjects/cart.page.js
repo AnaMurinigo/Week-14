e5787a0accd6222e24fbca275be45887dc1512d9
AnaMurinigo <anamurinigo@gmail.com>
2022-06-21 17:48:42 -0300

    Cart tests

--- test/pageobjects/cart.page.js
+++ test/pageobjects/cart.page.js
@@ -1,22 +1,7 @@
 class CartPage{
     //Getters
-    // get inputUsername() { return $('#user-name')}
-    get inventoryItemList() { return $$('.inventory_item_name')};
-    get removeBtnList() {return $$('.cart_button')};
     get continueShopBtn() {return $('#continue-shopping')};
     get checkOutBtn() {return $('#checkout')};
- 
-    //Setters
-    // async setUsername(username){
-    //     await this.inputUsername.setValue(username);
-    // }
-   
-    //Methods
-    // async login(username, password) {
-    //     await this.setUsername(username);
-    //     await this.setPassword(password);
-    //     await this.btnLogin.click();
-    // }
 }
 
 module.exports =new CartPage;
--- test/specs/cart.spec.js
+++ test/specs/cart.spec.js
@@ -6,36 +6,24 @@ describe('cart test', () => {
     beforeAll('open browser', () => {
         browser.url('https://www.saucedemo.com/')
         LoginPage.login('standard_user','secret_sauce');
-        InventoryPage.addToCart(1);
-        InventoryPage.addToCart(2);
+        for (let i = 1; i < 7; i++) {
+            InventoryPage.addToCart(i);
+        }
         InventoryPage.cartBtn.click();
     })
 
-    it('Adding succesfull', async () => {
-        // await InventoryPage.removeBtnList[0].click();
-        await expect(InventoryPage.cartItems).toHaveText('2');
-        // await expect(InventoryPage.removeBtn).toHaveText('REMOVE');
+    it('Removing succesfull', async () => {
+        await expect(InventoryPage.removeBtn1).toHaveText('REMOVE');
+        await InventoryPage.removeFromCart(1);
+        await expect(InventoryPage.cartItems).toHaveText('5');
+     })
+     it('Continue shopping test', async () => {
+        await CartPage.continueShopBtn.click();
+        await expect(LoginPage.title).toHaveText('PRODUCTS');
+     })
+     it('Check out test', async () => {
+        await InventoryPage.cartBtn.click();
+        await CartPage.checkOutBtn.click();
+        await expect(LoginPage.title).toHaveText('CHECKOUT: YOUR INFORMATION');
      })
 })
-// describe('cart test', () => {
-
-//     beforeAll('open browser', () => {
-//         browser.url('https://www.saucedemo.com/')
-//         LoginPage.login('standard_user','secret_sauce');
-//         InventoryPage.addBtnList[0].click();
-//         InventoryPage.addBtnList[2].click();
-//         InventoryPage.cartBtn.click();
-//     })
-
-//     it('Adding succesfull', async () => {
-//         await CartPage.removeBtnList[0].click();
-//         await expect(InventoryPage.cartItems).toHaveText('1');
-//     //     await expect(InventoryPage.removeBtn).toHaveText('REMOVE');
-//      })
-    // it('Removing succesfull', async () => {
-    //     await expect(InventoryPage.removeBtn).toHaveText('REMOVE');
-    //     await InventoryPage.removeFromCart();
-    //     await expect(InventoryPage.cartItems).not.toBeExisting();
-    //     await expect(InventoryPage.addBtn).toHaveText('ADD TO CART');
-    // })
-// })
