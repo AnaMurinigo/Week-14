58f066ddbd75842daf66c1ce6a6edf407e505391
AnaMurinigo <anamurinigo@gmail.com>
2022-06-21 19:59:06 -0300

    Checkout 2 tests

---
+++ test/pageobjects/checkout2.page.js
@@ -0,0 +1,7 @@
+class Checkout2{
+    //Getters
+    get finishBtn() {return $('#finish')};
+    get backHomeBtn() {return $('#back-to-products')};
+}
+
+module.exports =new Checkout2;
--- test/specs/checkout.spec.js
+++ test/specs/checkout.spec.js
@@ -1,7 +1,8 @@
 const CheckoutPage = require('../pageobjects/checkout.page');
-const InventoryPage = require('../pageobjects/inventory.page')
-const LoginPage = require('../pageobjects/login.page')
-const CartPage = require('../pageobjects/cart.page')
+const InventoryPage = require('../pageobjects/inventory.page');
+const LoginPage = require('../pageobjects/login.page');
+const CartPage = require('../pageobjects/cart.page');
+const Checkout2 =  require('../pageobjects/checkout2.page');
 
 afterEach ('Reload page', ()=> {
         browser.url('https://www.saucedemo.com/checkout-step-one.html');
@@ -32,7 +33,17 @@ describe('cart test', () => {
     it('valid data should redirect', async () => {
         await CheckoutPage.entryData('Pepe','Lui', '1234');
         await expect(browser).toHaveUrl('https://www.saucedemo.com/checkout-step-two.html');
-        await finishBtn.click();
+    })
+    it('Complete buy', async () => {
+        await CheckoutPage.entryData('Pepe','Lui', '1234');
+        await Checkout2.finishBtn.click();
         await expect(browser).toHaveUrl('https://www.saucedemo.com/checkout-complete.html');
+        await Checkout2.backHomeBtn.click();
+        await expect(browser).toHaveUrl('https://www.saucedemo.com/inventory.html');
+    })
+    it('Press finish btn in checkout step two', async () => {
+        await CheckoutPage.entryData('Pepe','Lui', '1234');
+        await CheckoutPage.cancelBtn.click();
+        await expect(browser).toHaveUrl('https://www.saucedemo.com/inventory.html');
     })
 });
